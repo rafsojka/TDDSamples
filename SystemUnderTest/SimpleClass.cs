f6d394b9922d64b9622d4410f72bdee1293540d9
rafsojka <raf.sojka@gmail.com>
2017-12-08 21:38:47 +0000

    NUnit Assert.Throws

---
+++ SystemUnderTest/SimpleClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SystemUnderTest
+{
+    public class SimpleClass
+    {
+        public string SimpleMethod(string input)
+        {
+            if (input == "input1")
+            {
+                return "input 1";
+            }
+            else if (input == "input2")
+            {
+                return "input 2";
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ Tests/SimpleClassTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using SystemUnderTest;
+
+namespace Tests
+{
+    [TestFixture]
+    public class SimpleClassTests
+    {
+        [TestCase("input1", "input 1")]
+        [TestCase("input2", "input 2")]
+        public void SimpleMethod_Returns(string input, string expectedOutput)
+        {
+            var simpleClass = new SimpleClass();
+            Assert.That(expectedOutput, Is.EqualTo(simpleClass.SimpleMethod(input)));
+        }
+
+        [Test]
+        public void SimpleMethod_Throws()
+        {
+            var simpleClass = new SimpleClass();
+
+            //Exception ex = Assert.Throws<Exception>(
+            //    delegate { simpleClass.SimpleMethod("fakeString"); });
+
+            Exception ex = Assert.Throws<Exception>(() => simpleClass.SimpleMethod("fakeString"));
+
+            Assert.That(ex, Is.TypeOf(typeof(Exception)));
+        }
+
+    }
+}
