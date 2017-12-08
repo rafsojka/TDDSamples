1cb22811f25ffa513065012f9b9d7fc077d63d89
rafsojka <raf.sojka@gmail.com>
2017-12-08 22:10:07 +0000

    simple Moq

---
+++ SystemUnderTest/ClassWithDependency.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemUnderTest
+{
+    public interface IPriceGenerator
+    {
+        int GeneratePrice();
+    }
+
+    public class PriceGenerator : IPriceGenerator
+    {
+        public int GeneratePrice()
+        {
+            var rnd = new Random();
+            return rnd.Next(-100, 150);
+        }
+    }
+
+    public class ClassWithDependency
+    {
+        private IPriceGenerator priceGenerator;
+
+        public ClassWithDependency(IPriceGenerator priceGenerator)
+        {
+            this.priceGenerator = priceGenerator;
+        }
+
+        public bool VerifyPrice()
+        {
+            return (priceGenerator.GeneratePrice() > 0 && priceGenerator.GeneratePrice() <= 100);
+        }
+    }
+}
---
+++ Tests/ClassWithDependencyTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SystemUnderTest;
+using Moq;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ClassWithDependencyTests
+    {
+        [TestCase(-30, false)]
+        [TestCase(0, false)]
+        [TestCase(1, true)]
+        [TestCase(77, true)]
+        [TestCase(100, true)]
+        [TestCase(135, false)]
+        public void VerifyPrice_Returns(int priceReturned, bool expectedResult)
+        {
+            var mock = new Mock<IPriceGenerator>();
+            mock.Setup(foo => foo.GeneratePrice()).Returns(priceReturned);
+
+            var classWithDependency = new ClassWithDependency(mock.Object);
+
+            Assert.AreEqual(expectedResult, classWithDependency.VerifyPrice());
+        }
+    }
+}
