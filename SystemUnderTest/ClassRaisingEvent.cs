9faeb5f681bcc7e4311ee39cbc9130f21314c61c
rafsojka <raf.sojka@gmail.com>
2018-01-25 19:13:37 +0000

    event gets raised test

---
+++ SystemUnderTest/ClassRaisingEvent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemUnderTest
+{
+    interface IClassRaisingEvent
+    {
+        event EventHandler<PriceVerifiedEventArgs> PriceVerified;
+    }
+
+    public class ClassRaisingEvent : IClassRaisingEvent
+    {
+        private IPriceGenerator priceGenerator;
+
+        public ClassRaisingEvent(IPriceGenerator priceGenerator)
+        {
+            this.priceGenerator = priceGenerator;
+        }
+
+        public void VerifyPrice()
+        {
+            var price = priceGenerator.GeneratePrice();
+            if (price > 0 && price <= 100)
+            {
+                PriceVerifiedEventArgs args = new PriceVerifiedEventArgs();
+                args.Price = price;
+                OnPriceVerified(args);
+            }
+        }
+
+        protected virtual void OnPriceVerified(PriceVerifiedEventArgs e)
+        {
+            EventHandler<PriceVerifiedEventArgs> handler = PriceVerified;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event EventHandler<PriceVerifiedEventArgs> PriceVerified;
+    }
+
+    public class PriceVerifiedEventArgs : EventArgs
+    {
+        
+        public int Price { get; set; }
+    }
+}
---
+++ Tests/ClassRaisingEventTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using Moq;
+
+using SystemUnderTest;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ClassRaisingEventTests
+    {
+        [TestCase(-30, 0)]
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(77, 1)]
+        [TestCase(100, 1)]
+        [TestCase(135, 0)]
+        // based on https://stackoverflow.com/a/249042
+        public void VerifyPrice_Raises_PriceVerified(int priceReturned, int expectedCount)
+        {
+            // Arrange
+            var mock = new Mock<IPriceGenerator>();
+            mock.Setup(foo => foo.GeneratePrice()).Returns(priceReturned);
+
+            var classRaisingEvent = new ClassRaisingEvent(mock.Object);
+
+            var receivedEvents = new List<PriceVerifiedEventArgs>();
+            classRaisingEvent.PriceVerified += (s, e) => receivedEvents.Add(e);
+
+            // Act
+            classRaisingEvent.VerifyPrice();
+
+            // Assert
+            Assert.That(receivedEvents.Count, Is.EqualTo(expectedCount));
+            if (receivedEvents.Count > 0)
+            {
+                Assert.That(receivedEvents[0].Price, Is.EqualTo(priceReturned));
+            }
+        }
+    }
+}
